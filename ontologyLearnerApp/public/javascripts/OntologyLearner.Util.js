bd97577b07586c0bc8ea64b6f60563713105edf7
Mus'ab Husaini <musabhusaini@gmail.com>
2012-01-05 19:05:47 +0200

    MHusaini - Checking in a basic working web app for ontology learner.

--- ontologyLearnerApp/public/javascripts/OntologyLearner.Util.js
+++ ontologyLearnerApp/public/javascripts/OntologyLearner.Util.js
@@ -2,11 +2,9 @@
 	OntologyLearner.Util = OntologyLearner.Util || {};
 
 	OntologyLearner.Util.displaySpinner = function(element) {
-		var spinner = document.createElement("img");
-		$(spinner).attr("src", UrlStore.GetSpinner());
 		$(element)
 			.empty()
-			.append(spinner);
+			.append("<img src='" + UrlStore.GetSpinner() + "' />");
 	};
 	
 })(window, window.document, window.jQuery, window.OntologyLearner, window.OntologyLearner.UrlStore);
